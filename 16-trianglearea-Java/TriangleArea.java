82a82b09cb6bc92626715537627ecb44e5402456
ndvssankar <sivashankar@msitprogram.net>
2021-06-07 19:25:28 +0530

    triangle area Java Done

--- 16-trianglearea-Java/TriangleArea.java
+++ 16-trianglearea-Java/TriangleArea.java
@@ -7,6 +7,7 @@
 public class TriangleArea {
 	public int triangleArea(double s1, double s2, double s3) {
 		// Your code goes here
-		return 216;
+		double s = (s1 + s2 + s3) / 2;
+		return (int)Math.round(Math.sqrt(s * (s-s1) * (s-s2) * (s-s3)));
 	}
 }
